ef5b458cfc2fc7b31cf6ba519ed95c4504ba2a57
farisazizi <azizi.faris@gmail.com>
2015-06-22 11:00:49 +0000

    finish login/logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-   test "invalid signup information" do
+  
+    test "invalid signup information" do
      get signup_path
      assert_no_difference 'User.count' do
        post users_path, user: {name: "",
@@ -18,11 +19,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         get signup_path
         assert_difference 'User.count', 1 do
             post_via_redirect users_path, user: { name: "Example User",
-                                                email: "user@example.com",
-                                                password: "password",
-                                                password_confirmation: "password" }
+                                                  email: "user@example.com",
+                                                  password: "password",
+                                                  password_confirmation: "password" }
         end
-    assert_template 'users/show'
-    assert is_logged_in?
+        assert_template 'users/show'
+        assert is_logged_in?
     end
 end
