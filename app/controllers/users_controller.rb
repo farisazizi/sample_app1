e061ed08831df0e877194df8905f5d047e4d3f28
farisazizi <azizi.faris@gmail.com>
2015-07-02 11:41:25 +0000

    exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless (@user && @user.activated? &&
+                                  @user.authenticated?(:reset, params[:id]))
   end
   
   def new
@@ -26,7 +28,8 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.paginate(page: params[:page])
+    #@users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def edit
